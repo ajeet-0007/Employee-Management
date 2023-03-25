0d0ecff0304d46e29321be87838c692803a2cb75
Prajjwal <prajjwalpathak.asm@gmail.com>
2023-03-25 17:23:01 +0530

    Database Configuration

--- models/index.js
+++ models/index.js
@@ -1,14 +1,16 @@
 require("dotenv").config();
-const { Sequelize } = require("sequelize");
+const { Sequelize, DataTypes } = require("sequelize");
+const DB_NAME = process.env.DB_NAME;
+const DB_HOST = process.env.DB_HOST;
+const DB_USERNAME = process.env.DB_USERNAME;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_PORT = process.env.DB_PORT;
 
-const sequelize = new Sequelize("organizationdb", "root", "12345", {
-    host: "localhost",
+const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
+    host: DB_HOST,
     dialect: "mssql",
-    port: "1433",
+    port: DB_PORT,
     logging: false,
-    define: {
-        timestamps: false,
-    },
 });
 
 try {
