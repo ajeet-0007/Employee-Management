1a0effee1e226e050235658518623b9adfc51b91
Prajjwal Pathak <prajjwalpathak1105@gmail.com>
2023-04-06 16:49:22 +0530

    Updated attendance table, added checkInLocation and checkOutLocation

--- models/userAttendance.js
+++ models/userAttendance.js
@@ -1,23 +1,26 @@
 module.exports = (sequelize, DataTypes) => {
-  const USERATTENDANCE = sequelize.define("userAttendance", {
-    userId: {
-      type: DataTypes.INTEGER,
-    },
-    checkInTime: {
-      type: DataTypes.TIME,
-    },
-    checkOutTime: {
-      type: DataTypes.TIME,
-    },
-    checkInDate: {
-      type: DataTypes.DATEONLY,
-    },
-    checkOutDate: {
-      type: DataTypes.DATEONLY,
-    },
-    location: {
-      type: DataTypes.STRING,
-    }
-  });
-  return USERATTENDANCE;
+	const USERATTENDANCE = sequelize.define('userAttendance', {
+		userId: {
+			type: DataTypes.INTEGER,
+		},
+		checkInTime: {
+			type: DataTypes.TIME,
+		},
+		checkOutTime: {
+			type: DataTypes.TIME,
+		},
+		checkInDate: {
+			type: DataTypes.DATEONLY,
+		},
+		checkOutDate: {
+			type: DataTypes.DATEONLY,
+		},
+		checkInLocation: {
+			type: DataTypes.STRING,
+		},
+		checkOutLocation: {
+			type: DataTypes.STRING,
+		},
+	});
+	return USERATTENDANCE;
 };
