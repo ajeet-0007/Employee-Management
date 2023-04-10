01ce953ca560f4bae26b250ec1e59b926fa6d1ea
Prajjwal Pathak <prajjwalpathak1105@gmail.com>
2023-04-10 15:24:32 +0530

    Add admin/get-all-users route

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(cors({ origin: ['http://localhost:3000', 'http://127.0.0.1:5500'], credentials: true }));
+app.use(cors({ origin: ['http://localhost:3000', 'http://localhost:5173'], credentials: true }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
