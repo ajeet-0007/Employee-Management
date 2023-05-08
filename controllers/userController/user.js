241ecc88527a2b3bc03465437929cdd27cfc8a81
Prajjwal Pathak <prajjwalpathak1105@gmail.com>
2023-05-08 12:10:01 +0530

    Updated userProfile

--- controllers/userController/user.js
+++ controllers/userController/user.js
@@ -140,7 +140,7 @@ exports.getSearchedUser = async (req, res) => {
 		if (userSubordinateData[0].length !== 0) {
 			userData.subordinates = userSubordinateData[0];
 		}
-		return res.status(200).json([userData]);
+		return res.status(200).json(userData);
 	} catch (error) {
 		console.log(error);
 		return res.status(500).json({
