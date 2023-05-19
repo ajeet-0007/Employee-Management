d209c2246dd69524bfb0852ec713842e6cd0541e
Prajjwal Pathak <prajjwalpathak1105@gmail.com>
2023-05-19 11:22:55 +0530

    Updated notification

--- controllers/functions/userTimesheet.js
+++ controllers/functions/userTimesheet.js
@@ -4,12 +4,13 @@ const { randomBytes } = require('crypto');
 
 const createTimesheetNotification = async (userId) => {
 	const userData = await getUser(userId);
-	await db.sequelize.query('EXEC dbo.spusers_postusernotification :notification_id, :content, :sender, :receiver, :type', {
+	await db.sequelize.query('EXEC dbo.spusers_postusernotification :notification_id, :content, :sender, :receiver, :date, :type', {
 		replacements: {
 			notification_id: randomBytes(16).toString('hex'),
 			content: `A timesheet is waiting for your approval from ${userData.name}.`,
 			sender: userData.hrmid,
 			receiver: userData.reportsTo,
+			date: new Date(),
 			type: 'timesheet'
 		}
 	});
@@ -18,12 +19,13 @@ const createTimesheetNotification = async (userId) => {
 const updateTimesheetNotification = async (senderId, receiverId, status) => {
 	const sender = await getUser(senderId); //user who approves the request
 	const receiver = await getUser(receiverId); //user who sent the request
-	await db.sequelize.query('EXEC dbo.spusers_postusernotification :notification_id, :content, :sender, :receiver, :type', {
+	await db.sequelize.query('EXEC dbo.spusers_postusernotification :notification_id, :content, :sender, :receiver, :date, :type', {
 		replacements: {
 			notification_id: randomBytes(16).toString('hex'),
 			content: `Your timesheet has been ${status.toLowerCase()}ed by ${sender.name}.`,
 			sender: sender.hrmid,
 			receiver: receiver.hrmid,
+			date: new Date(),
 			type: 'timesheet'
 		}
 	});
