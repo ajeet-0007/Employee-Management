98c629f185174efa913a5d58b9ca961f7d4d4575
Prajjwal Pathak <prajjwalpathak1105@gmail.com>
2023-06-26 16:55:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(cors({ origin: ['http://localhost:3000'], credentials: true }));
+app.use(cors({ origin: ['http://localhost:3000', 'https://emergent-ventures.vercel.app'], credentials: true }));
 app.use(bodyParser.urlencoded({ extended: false, limit: '50mb' }));
 app.use(express.json({ limit: '50mb' }));
 app.use(cookieParser());
